cfc6c61b0d663e7bcd07eb1cbd93c44e8d73436e
Fausto Gabini <fausgabini@gmail.com>
2021-12-04 20:20:01 -0300

    add horarios command

--- bot.js
+++ bot.js
@@ -1,17 +1,109 @@
 require("dotenv").config();
 var chistes = require("./api/chistes.json");
+const paradas = require("./api/paradas.json");
 const { Telegraf } = require("telegraf");
 const axios = require("axios");
 
 const bot = new Telegraf(process.env.TOKEN_API);
 
+const lineasColectivo = [
+  [
+    {
+      text: "227",
+      callback_data: "501",
+    },
+    {
+      text: "230",
+      callback_data: "501",
+    },
+    {
+      text: "342",
+      callback_data: "501",
+    },
+  ],
+  [
+    {
+      text: "374",
+      callback_data: "501",
+    },
+    {
+      text: "375",
+      callback_data: "501",
+    },
+    {
+      text: "500",
+      callback_data: "501",
+    },
+  ],
+  [
+    {
+      text: "501",
+      callback_data: "501",
+    },
+    {
+      text: "502",
+      callback_data: "501",
+    },
+    {
+      text: "503",
+      callback_data: "501",
+    },
+  ],
+  [
+    {
+      text: "504",
+      callback_data: "501",
+    },
+    {
+      text: "505",
+      callback_data: "501",
+    },
+    {
+      text: "506",
+      callback_data: "501",
+    },
+  ],
+  [
+    {
+      text: "507",
+      callback_data: "501",
+    },
+    {
+      text: "510",
+      callback_data: "501",
+    },
+    {
+      text: "512",
+      callback_data: "501",
+    },
+  ],
+
+  [
+    {
+      text: "342 A",
+      callback_data: "501",
+    },
+    {
+      text: "342 B",
+      callback_data: "501",
+    },
+    {
+      text: "342 C",
+      callback_data: "501",
+    },
+  ],
+];
+
 bot.start((ctx) => {
   ctx.reply(
     `¡Hola ${ctx.from.first_name}! 👋 Soy Nico, el chatbot no oficial de San Nicolas de los Arroyos.`
   );
+  ctx.replyWithSticker(
+    "https://res.cloudinary.com/dgq6qxh4k/image/upload/v1638586589/TelegramBot/a4f89a0e-a2b3-4713-be45-9924cfdca0ee_k3phqv.webp"
+  );
   setTimeout(() => {
     ctx.reply(
-      `¿Por dónde empezamos? 👇 \n A. Farmacias de turno \n B. Numeros de emergencia 📞 \n C. Paradas de colectivos 🚌 \n D. Contame un chiste 🎉`
+      `¿Por dónde empezamos? 👇 \n A. Farmacias de turno \n B. Numeros de emergencia 📞 \n C. Horarios de colectivos 🚌 \n D. Contame un chiste 🎉`
     );
   }, 1000);
 });
@@ -80,9 +172,26 @@ bot.hears(
 );
 
 bot.hears(
-  ["C", "c", "paradas", "Paradas", "Paradas de colectivo"],
+  [
+    "C",
+    "c",
+    "horarios",
+    "Horarios",
+    "Horarios de colectivo",
+    "Horarios cole",
+  ],
   (ctx) => {
-    ctx.reply("🚧 El comando está en desarrollo 🏗️");
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      "Estas son las lineas de colectivos disponibles",
+      {
+        reply_markup: {
+          inline_keyboard: lineasColectivo,
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      }
+    );
   }
 );
 
@@ -97,7 +206,23 @@ bot.hears(
     setTimeout(() => {
       ctx.reply(chisteAleatorio.response);
     }, 1000);
+    ctx.replyWithSticker(
+      "https://res.cloudinary.com/dgq6qxh4k/image/upload/v1638586844/TelegramBot/fe4d2934-e495-435b-a7c1-517a3bbd469b_tkivvh.webp"
+    );
   }
 );
+
+bot.action(["501"], (ctx) => {
+  ctx.reply("Estos son los horarios de la linea 501");
+  ctx.telegram
+    .sendDocument(ctx.from.id, {
+      source: "./assets/501.pdf",
+      filename: "Horarios501.pdf",
+    })
+    .catch(function (error) {
+      console.log(error);
+      ctx.reply("Lo siento no encontre los horarios");
+    });
+});
 
 bot.launch();
